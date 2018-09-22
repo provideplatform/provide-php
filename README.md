7ba26c0a3849c7201cf07397be3db45ae3abbb6d
bwentzloff <brian.wentzloff@gmail.com>
2018-09-22 10:01:44 -0400

    getting started

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # provide-php
+
+This is the PHP library for interacting with https://provide.services
+
+A Postman collection is published here for the API: 
+
+Here are the implemented endpoints:
+
+```php
+getContracts
+```
+
---
+++ src/api_client.php
@@ -0,0 +1,26 @@
+class APIClient {
+
+    DEFAULT_SCHEME = 'https';
+    DEFAULT_VERSION = 'v1';
+
+    function __construct($var) {
+
+    }
+
+    function get(uri, params) {
+
+    }
+
+    function post(uri, params) {
+
+    }
+
+    function put(uri, params) {
+        
+    }
+
+    function delete(uri) {
+
+    }
+
+}
